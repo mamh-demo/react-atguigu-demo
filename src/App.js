e863e8d3f278167c68d894baa4f4c3381296534b
bright.ma <bright.ma@blackshark.com>
2022-09-18 10:56:12 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Hello from "./components/Hello";
-import Ailala from "./components/Ailala"
+import Hello from "./components/Hello/Hello";
+import Ailala from "./components/Ailala/Ailala"
 
 export default class App extends React.Component {
 
