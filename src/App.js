41bc69282b0e2b946b2b92b4ebeff4dd3806f118
bright.ma <bright.ma@blackshark.com>
2022-09-19 21:53:05 +0800

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,107 +1,58 @@
 import React from 'react'
-import {nanoid} from 'nanoid'
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import List from "./components/List";
+
 import './App.css'
-import Ailala from "./components/Ailala/Ailala";
 
 
 export default class App extends React.Component {
-    state = {
-        todos: [
-            {id: nanoid(), name: 'chifan', done: true},
-            {id: nanoid(), name: 'shuijiao', done: true},
-            {id: nanoid(), name: 'docode', done: false},
-            {id: nanoid(), name: 'guangjie', done: false},
-            {id: nanoid(), name: 'ailala', done: false},
-        ]
-    }
-
-    addTodo = (obj) => {
-        const {todos} = this.state
-        const newtodos = [obj, ...todos]
-        this.setState({
-            todos: newtodos
-        })
-    }
-
-    updateTodo = (id, done) => {
-        const {todos} = this.state
-        const newtodos = todos.map((todo) => {
-            if (todo.id === id) {
-                return {
-                    ...todo, done: done
-                }
-            } else {
-                return {
-                    ...todo
-                }
-            }
-        })
-
-        this.setState({
-            todos: newtodos
-        })
-    }
-
-    checkAllTodo = (d) => {
-        const {todos} = this.state
-        const newtodos = todos.map((todo) => {
-            return {
-                ...todo, done: d
-            }
-
-        })
-
-        this.setState({
-            todos: newtodos
-        })
-
-
-    }
-
-
-    deleteTodo = (id) => {
-        const {todos} = this.state
-        const newtodos = todos.filter((todo) => {
-            return todo.id !== id
-        })
-
-        this.setState({
-            todos: newtodos
-        })
-
-    }
-
-    removeAllTodo = () => {
-        const {todos} = this.state
-        const newtodos = todos.filter((todo) => {
-            return !todo.done
-        })
-
-        this.setState({
-            todos: newtodos
-        })
-    }
 
 
     render() {
         return (
-            <div className="todo-container">
-                <div className="todo-wrap">
-                    <Header addTodo={this.addTodo}/>
-                    <List todos={this.state.todos}
-                          updateTodo={this.updateTodo}
-                          deleteTodo={this.deleteTodo}
-                    />
-                    <Footer todos={this.state.todos}
-                            checkAllTodo={this.checkAllTodo}
-                            removeAllTodo={this.removeAllTodo}
-
-                    />
-                    <Ailala/>
+            <div className="container">
+                <section className="jumbotron">
+                    <h3 className="jumbotron-heading">Search Github Users</h3>
+                    <div>
+                        <input type="text" placeholder="enter the name you search"/>&nbsp;
+                        <button>Search</button>
+                    </div>
+                </section>
+                <div className="row">
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
+                                 style={{width: '100px'}}/>
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
+                                 style={{width: '100px'}}/>
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
+                                 style={{width: '100px'}}/>
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
+                                 style={{width: '100px'}}/>
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
+                    <div className="card">
+                        <a href="https://github.com/reactjs" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
+                                 style={{width: '100px'}}/>
+                        </a>
+                        <p className="card-text">reactjs</p>
+                    </div>
                 </div>
             </div>
         )
