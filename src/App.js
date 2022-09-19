c52b8c7ddb3fc635cee7be8d5e293f9231da631a
bright.ma <bright.ma@blackshark.com>
2022-09-19 22:42:16 +0800

    update add App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import Search from "./components/Search";
+
+import List from "./components/List";
+
 
 import './App.css'
 
@@ -10,50 +14,8 @@ export default class App extends React.Component {
     render() {
         return (
             <div className="container">
-                <section className="jumbotron">
-                    <h3 className="jumbotron-heading">Search Github Users</h3>
-                    <div>
-                        <input type="text" placeholder="enter the name you search"/>&nbsp;
-                        <button>Search</button>
-                    </div>
-                </section>
-                <div className="row">
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank">
-                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
-                                 style={{width: '100px'}}/>
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank">
-                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
-                                 style={{width: '100px'}}/>
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank">
-                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
-                                 style={{width: '100px'}}/>
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank">
-                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
-                                 style={{width: '100px'}}/>
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                    <div className="card">
-                        <a href="https://github.com/reactjs" target="_blank">
-                            <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
-                                 style={{width: '100px'}}/>
-                        </a>
-                        <p className="card-text">reactjs</p>
-                    </div>
-                </div>
+                <Search/>
+                <List/>
             </div>
         )
     }
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,25 +1,47 @@
 import React from 'react'
-import Item from "../Item";
-import PropTypes from "prop-types";
 
 export default class List extends React.Component {
-    static propTypes = {
-        todos: PropTypes.array.isRequired,
-        updateTodo: PropTypes.func.isRequired,
-        deleteTodo: PropTypes.func.isRequired
-    }
 
     render() {
         const {todos, updateTodo, deleteTodo} = this.props
         return (
-            <ul className="todo-main">
-                {
-                    todos.map((todo) => {
-                        return <Item key={todo.id} {...todo} updateTodo={updateTodo} deleteTodo={deleteTodo}
-                        />
-                    })
-                }
-            </ul>
+            <div className="row">
+                <div className="card">
+                    <a href="https://github.com/reactjs" target="_blank">
+                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
+                             style={{width: '100px'}}/>
+                    </a>
+                    <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+                    <a href="https://github.com/reactjs" target="_blank">
+                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
+                             style={{width: '100px'}}/>
+                    </a>
+                    <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+                    <a href="https://github.com/reactjs" target="_blank">
+                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
+                             style={{width: '100px'}}/>
+                    </a>
+                    <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+                    <a href="https://github.com/reactjs" target="_blank">
+                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
+                             style={{width: '100px'}}/>
+                    </a>
+                    <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+                    <a href="https://github.com/reactjs" target="_blank">
+                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3"
+                             style={{width: '100px'}}/>
+                    </a>
+                    <p className="card-text">reactjs</p>
+                </div>
+            </div>
         )
     }
 
