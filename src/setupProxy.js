1623efb08c2698c9820c384419329103ade6a544
bright.ma <bright.ma@blackshark.com>
2022-09-19 21:47:09 +0800

    update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware('/api1', {
+        createProxyMiddleware('/api1', {// 遇见 /api1 前缀的路径 才会走这个 代理
             target: 'http://localhost:5000',
             changeOrigin:true,
             pathRewrite:{
