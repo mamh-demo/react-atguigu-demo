b937b2015a6de747f06235055ed223cdca8ade55
bright.ma <bright.ma@blackshark.com>
2022-09-24 12:33:15 +0800

    add src/components/Home

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './index.css'
+
+export default class Home extends React.Component {
+    render() {
+
+        return (
+            <div>
+                home.............
+            </div>
+        )
+    }
+
+
+
+}
+
+
+
+
