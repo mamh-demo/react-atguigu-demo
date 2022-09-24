3cbf85fc2442bdadfd44a523b21169d82fbb6342
bright.ma <bright.ma@blackshark.com>
2022-09-24 11:26:02 +0800

    add fetch

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -11,14 +11,17 @@ export default class Search extends React.Component {
                 <h3 className="jumbotron-heading">Search Github Users</h3>
                 <div>
                     <input ref={c => this.keyWordElement = c} type="text"
-                           placeholder="enter the name you search"/>&nbsp;
-                    <button onClick={this.search}>Search</button>
+                           placeholder="enter the name you search"/>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button onClick={this.searchGithub}>Search</button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <button onClick={this.fetchGithub}>Search</button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                 </div>
             </section>
         )
     }
 
-    search = () => {
+    searchGithub = () => {
 
 
         //js的解构赋值
@@ -33,6 +36,60 @@ export default class Search extends React.Component {
                 PubSub.publish('ailala', {err: error.message, isLoading: false})
             }
         )
+
+
+    }
+
+    fetchGithub = () => {
+
+
+        //js的解构赋值
+        const {keyWordElement: {value: keyWord}} = this
+        PubSub.publish('ailala', {isFirst: false, isLoading: true})
+
+        // fetch(`http://localhost:3000/api1/search/users2?q=${keyWord}`).then(
+        //     response => {
+        //         return response.json()
+        //     },
+        //     error => {
+        //         return new Promise(()=>{ console.log(error)})
+        //     }
+        // ).then(
+        //     response => {
+        //         console.log(response)
+        //         PubSub.publish('ailala', {users: response.items, isLoading: false})
+        //     },
+        //     error => {
+        //         console.log(error)
+        //         PubSub.publish('ailala', {err: error.message, isLoading: false})
+        //     }
+        // )
+
+
+        fetch(`http://localhost:3000/api1/search/users2?q=${keyWord}`).then(
+            response => {
+                return response.json()
+            },
+        ).then(
+            response => {
+                console.log(response)
+                PubSub.publish('ailala', {users: response.items, isLoading: false})
+            },
+        ).catch(
+            error => {
+                console.log(error)
+                PubSub.publish('ailala', {err: error.message, isLoading: false})
+            }
+        )
+
+        // try {
+        //     const response = await fetch(`http://localhost:3000/api1/search/users2?q=${keyWord}`)
+        //     const data = await response.json()
+        //     PubSub.publish('ailala', {users: data.items, isLoading: false})
+        // } catch (error) {
+        //     console.log(error)
+        //     PubSub.publish('ailala', {err: error.message, isLoading: false})
+        // }
 
 
     }
