028ac76522276f12c2e9836241ebcf9e82accb98
bright.ma <bright.ma@blackshark.com>
2022-09-24 12:29:05 +0800

    update App.js prepare for react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react'
 
-import Search from "./components/Search";
-
-import List from "./components/List";
-
-
 import './App.css'
 
 
@@ -14,8 +9,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div className="container">
-                <Search/>
-                <List/>
+
             </div>
         )
     }
