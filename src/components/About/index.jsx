ba0d4bdab9663c95ca80e51aed8085903ca1e282
bright.ma <bright.ma@blackshark.com>
2022-09-24 12:32:42 +0800

    add src/components/About

---
+++ src/components/About/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './index.css'
+
+export default class About extends React.Component {
+    render() {
+
+        return (
+            <div>
+                about.............
+            </div>
+        )
+    }
+
+
+
+}
+
+
+
+
