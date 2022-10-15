ac0373db8929cf69a0f8fd1029b3ed9d02df3162
bright.ma <bright.ma@blackshark.com>
2022-10-15 20:36:10 +0800

    add  components Redux

---
+++ src/components/Redux/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './index.css'
+
+
+
+export default class Redux extends React.Component {
+    render() {
+
+        return (
+<div>
+<h2>Redux组件内容</h2>
+<div>
+
+</div>
+</div>
+        )
+    }
+
+
+
+}
+
+
+
+
