deff45cc9a50533b5fc2b08bc0b50df778f50049
bright.ma <bright.ma@blackshark.com>
2022-10-16 20:46:54 +0800

    add combineReducers for countReducer/personReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,18 @@
-import { legacy_createStore as createStore, applyMiddleware} from 'redux'
+import { legacy_createStore as createStore, applyMiddleware, combineReducers} from 'redux'
 
 import thunk from 'redux-thunk'
 
 import countReducer from './reducers/count'
+import personReducer from "./reducers/person";
 
+const allReducers = combineReducers(
+    {
+        he: countReducer,
+        rens:  personReducer
+    }
+)
 
-const store = createStore(countReducer, applyMiddleware(thunk))
+const store = createStore(allReducers, applyMiddleware(thunk))
 
 
 export default store
