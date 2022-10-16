ed9ae9027f558929c201ee9dc60bee1e7fc762cc
bright.ma <bright.ma@blackshark.com>
2022-10-16 09:47:20 +0800

    add store.js

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { legacy_createStore as createStore} from 'redux'
+
+import countReducer from './count_reducer'
+
+
+
+const store = createStore(countReducer)
+
+
+
+export default store
+
+
