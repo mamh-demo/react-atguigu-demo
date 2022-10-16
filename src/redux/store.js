e056000c5c44a5e456e1f5ca74c9bef9d1621b45
bright.ma <bright.ma@blackshark.com>
2022-10-16 11:09:28 +0800

    add thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
-import { legacy_createStore as createStore} from 'redux'
+import { legacy_createStore as createStore, applyMiddleware} from 'redux'
+
+import thunk from 'redux-thunk'
 
 import countReducer from './count_reducer'
 
 
-
-const store = createStore(countReducer)
-
+const store = createStore(countReducer, applyMiddleware(thunk))
 
 
 export default store
