f65accc18ca3f086ad874e77592ec0798748576d
bright.ma <bright.ma@blackshark.com>
2022-10-16 11:09:16 +0800

    export const createIncrementAsyncAction

--- src/redux/count_action.js
+++ src/redux/count_action.js
@@ -12,3 +12,12 @@ import {DECREMENT, INCREMENT} from './constant'
 export const createIncrementAction = data => ({type: INCREMENT, data})
 export const createDecrementAction = data => ({type: DECREMENT, data})
 
+export const createIncrementAsyncAction = (data, time) => {
+    return (dispatch) => {
+        setTimeout(()=>{
+            dispatch(createIncrementAction(data))
+        }, time)
+    }
+
+
+}
