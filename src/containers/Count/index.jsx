c84d02d097c37c037d1e822cd244b2548aaff87c
bright.ma <bright.ma@blackshark.com>
2022-10-16 13:45:07 +0800

    add mapStateToProps mapDispatchToProps

--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -2,9 +2,32 @@ import CountUI from '../../components/Redux/CountUI'
 
 
 import {connect} from 'react-redux'
+import {
+    createDecrementAction,
+    createIncrementAction,
+    createIncrementAsyncAction
+} from "../../redux/count_action";
 
 
-const CountContainer = connect()(CountUI)
+function mapStateToProps(state) {
+    return {count: state}
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        jia: (num) => {
+            dispatch(createIncrementAction(num))
+        },
+        jian: (num) => {
+            dispatch(createDecrementAction(num))
+        },
+        asyncjia: (num, time) => {
+            dispatch(createIncrementAsyncAction(num, time))
+        },
+    }
+}
+
+const CountContainer = connect(mapStateToProps, mapDispatchToProps)(CountUI)
 
 
 export default CountContainer
