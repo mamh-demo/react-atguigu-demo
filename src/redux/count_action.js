8e467bbc3a613b91b160f13de585517fe3453f03
bright.ma <bright.ma@blackshark.com>
2022-10-16 10:01:59 +0800

    add count_action.js

---
+++ src/redux/count_action.js
@@ -0,0 +1,12 @@
+//
+// function createIncrementAction(data){
+//     return {type:'increment', data}
+// }
+//
+// function createDecrementAction(data){
+//     return {type:'decrement', data}
+// }
+
+export const createIncrementAction = data => ({type:'increment', data})
+export const createDecrementAction = data => ({type:'decrement', data})
+
