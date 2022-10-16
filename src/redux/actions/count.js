b6ffe55a2f7bdec788b33022bf2d6869414116b2
bright.ma <bright.ma@blackshark.com>
2022-10-16 19:34:32 +0800

    rename to actions/count.js

--- src/redux/actions/count.js
+++ src/redux/actions/count.js
@@ -7,7 +7,7 @@
 //     return {type:'decrement', data}
 // }
 
-import {DECREMENT, INCREMENT} from './constant'
+import {DECREMENT, INCREMENT} from '../constant'
 
 export const createIncrementAction = data => ({type: INCREMENT, data})
 export const createDecrementAction = data => ({type: DECREMENT, data})
