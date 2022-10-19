fc897c92f6d5bebe1882b61e1136fb6149e84052
bright.ma <bright.ma@blackshark.com>
2022-10-19 20:00:57 +0800

    add comments for index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import {Provider} from 'react-redux'
 import App from './App'
 
 ReactDOM.render(
+    /*
+    此处需要用provider包裹app，目的是让app所有的后代容器组件都能接收到store
+     */
     <Provider store={store}>
-    <App/>
+        <App/>
     </Provider>
-    , document.getElementById("root"))
+    ,
+    document.getElementById("root")
+)
 
 
 
